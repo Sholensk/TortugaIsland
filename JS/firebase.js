f83f7cbe41da544d3ba44387b93a360c6bb3a2cd
Viciouz Sholensk <cdmx2418@amerike.edu.mx>
2023-11-11 18:33:29 -0600

    instalacion de firebase

---
+++ JS/firebase.js
@@ -0,0 +1,15 @@
+import { InitializeApp} from "firebase/app";
+import {getFireStore} from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD9jFuzv_QZBLnhFE9rQVzg3sY48KQF4G0",
+    authDomain: "tortuga-island.firebaseapp.com",
+    projectId: "tortuga-island",
+    storageBucket: "tortuga-island.appspot.com",
+    messagingSenderId: "908568239741",
+    appId: "1:908568239741:web:1eacd5cdd7193287e71b14",
+    measurementId: "G-2P0GL15V8L"
+  };
+
+  
